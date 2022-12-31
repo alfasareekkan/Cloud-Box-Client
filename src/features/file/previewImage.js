bf5f69def89d6797fc2f8dabd4f501127c6c5f27
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2022-12-31 22:10:30 +0530

    the last commit of 2022

--- src/features/file/previewImage.js
+++ src/features/file/previewImage.js
@@ -6,14 +6,14 @@ import html2canvas from 'html2canvas';
 /* eslint-disable import/prefer-default-export */
 
 async function getImageData(element) {
-    // Render the element to a canvas
-    const canvas = await html2canvas(element);
-    // Extract the image data from the canvas
-    const imageData = canvas.toDataURL();
-    document.getElementById('xlsxDiv').style.display = 'none';   
-    return imageData;
+  // Render the element to a canvas
+  const canvas = await html2canvas(element);
+  // Extract the image data from the canvas
+  const imageData = canvas.toDataURL();
+  document.getElementById('xlsxDiv').style.display = 'none';
+  return imageData;
 }
-  
+
 async function generatePdfPreviewImage(file) {
   const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
   //     console.log(file);
@@ -31,14 +31,11 @@ async function generatePdfPreviewImage(file) {
     return page.render(
       { canvasContext: context, viewport: viewPort },
     ).promise.then((resolve) => {
-        
-        const imageData = canvas.toDataURL();
-        // document.getElementById('preview12345678').src = imageData;
-        // console.log(imageData);
-        return imageData
-    })
-    
-    
+      const imageData = canvas.toDataURL();
+      // document.getElementById('preview12345678').src = imageData;
+      // console.log(imageData);
+      return imageData;
+    });
   });
   //
 }
@@ -53,29 +50,26 @@ export const createPreviewImage = (file) => {
       const reader = new FileReader();
       let previewImage;
       reader.onload = async (e) => {
-          const a = e.target.result;
-          previewImage = await generatePdfPreviewImage(a);
+        const a = e.target.result;
+        previewImage = await generatePdfPreviewImage(a);
         resolve(previewImage);
       };
       reader.readAsDataURL(file);
     });
-  }
-  else if (file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || file.type === 'application/vnd.ms-excel') {
-      return new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onload = async (e) => {
-              console.log("sdfasdfsfasdf");
-              const data = e.target.result;
-              const workbook = XLSX.read(data, { type: 'binary' });
-              const sheet = workbook.Sheets[workbook.SheetNames[0]];
-              const html = XLSX.utils.sheet_to_html(sheet);
-              const container = document.getElementById('xlsxDiv');
-              container.innerHTML = html;
-              const imageData = await getImageData(container);
-              console.log(imageData);
-              resolve(imageData);
-          }
-          reader.readAsBinaryString(file);
-      })
+  } else if (file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || file.type === 'application/vnd.ms-excel') {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: 'binary' });
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+        const html = XLSX.utils.sheet_to_html(sheet);
+        const container = document.getElementById('xlsxDiv');
+        container.innerHTML = html;
+        const imageData = await getImageData(container);
+        resolve(imageData);
+      };
+      reader.readAsBinaryString(file);
+    });
   }
 };
