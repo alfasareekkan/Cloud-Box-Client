6800d541ca91aa88e4bc2c0382495f70c7fed0cd
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2023-01-03 19:27:07 +0530

    just began with rightmenu

--- src/components/Folder/Folder.jsx
+++ src/components/Folder/Folder.jsx
@@ -16,8 +16,14 @@ export default function Folder(props) {
     dispatch(insertPath(props.folder));
     navigate(`/dashboard/v1/${props?.folder._id}`, { replace: true });
   };
+  const handleRightClick = (event) => {
+    // Do something when the element is right-clicked
+    event.preventDefault(); // This will prevent the default right-click menu from appearing
+    alert('hi')
+  };
+
   return (
-    <div onClick={handleClick} className="ml-5 bg-[#ffffff] border-1 mt-5 h-10 w-11/12 rounded-md hover:bg-gray-100 md:w-4/12  lg:w-2/12 px-2  ">
+    <div onClick={handleClick} className="ml-5 bg-[#ffffff] border-1 mt-5 h-10 w-11/12 rounded-md hover:bg-gray-100 md:w-4/12  lg:w-2/12 px-2  " onContextMenu={handleRightClick}>
       <div className="flex ml-3 justify-start items-center">
         <BsFillFolderFill className="text-2xl text-[#9fa2f6] mt-2" />
         <p className="ml-4 text-[#5e6268] text-xs font-semibold mt-1">{props?.folder.folderName}</p>
--- src/features/Global/menuSlice.js
+++ src/features/Global/menuSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const createMenuSlice = createSlice({
   name: 'createMenu',
-  initialState: { createMenu: false },
+  initialState: { createMenu: false, rightClickMenu: false },
   reducers: {
     setCreateMenu: (state, action) => {
       // eslint-disable-next-line no-param-reassign
       state.createMenu = action.payload;
     },
+    setRightClickMenu: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      state.rightClickMenu = action.payload;
+    },
   },
 });
-export const { setCreateMenu } = createMenuSlice.actions;
+export const { setCreateMenu, setRightClickMenu } = createMenuSlice.actions;
 export default createMenuSlice.reducer;
