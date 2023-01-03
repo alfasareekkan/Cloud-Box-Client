f6459144c0847fa4c08529294c3b52fe4adf32e1
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2023-01-03 17:59:28 +0530

    encryption initial commit

--- src/features/Global/fileSizeSlice.js
+++ src/features/Global/fileSizeSlice.js
@@ -1,13 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
 
 const fileSizeSlice = createSlice({
-    name: "fileSize",
-    initialState: {
-        fileSize: 0,
+  name: 'fileSize',
+  initialState: {
+    fileSize: 0,
+  },
+  reducers: {
+      setFileSize: (state, action) => {
+        console.log(action.payload,"ff");
+      state.fileSize = action.payload;
     },
-    reducers: {
-        setFileSize: (state, action) => {
-            
-        }
-    }
-})
+  },
+});
+export const { setFileSize } = fileSizeSlice.actions;
+export default fileSizeSlice.reducer;
