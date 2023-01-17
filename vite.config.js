05735d3fa5411458e6cc4f18c60804467d033050
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2023-01-18 00:37:19 +0530

    fix confiqure bug in vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +9,13 @@ export default defineConfig({
       './runtimeConfig': './runtimeConfig.browser',
     },
   },
-})
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+});
 
 // import { defineConfig } from "vite";
 // import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
